efa58224abfd446cb59537b85d87a322c5289137
alexandre CPTM <alexandresbsantana@outlook.com>
2025-11-12 16:01:35 -0300

    chore: creation of default files and folders

---
+++ appfleshi/routes.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+from appfleshi import  app
+
+
+@app.route('/')
+def homepage():
+    return render_template('homepage.html')
+
+
+@app.route('/profile/<username>')
+def profile(username):
+    return render_template('profile.html', username=username)
