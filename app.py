32500421a5082e4ed1c87421fdb35bb4c20cb8c0
alexandre CPTM <alexandresbsantana@outlook.com>
2025-11-12 14:09:52 -0300

    chore: initial project configuration

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Seja Bem-Vindo ao Flask"
+
+if __name__ == '__main__':
+    app.run(debug=True)
