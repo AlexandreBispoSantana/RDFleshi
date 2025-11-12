7c5ac30d0e5c467162e354bda10c8b2f5897bab6
alexandre CPTM <alexandresbsantana@outlook.com>
2025-11-12 14:43:47 -0300

    feat: add homepage and profile pages

--- app.py
+++ app.py
@@ -4,7 +4,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Seja Bem-Vindo ao Flask"
+    return render_template('homepage.html')
+
+
+@app.route('/profile/<username>')
+def profile(username):
+    return render_template('profile.html', username=username)
 
 if __name__ == '__main__':
     app.run(debug=True)
